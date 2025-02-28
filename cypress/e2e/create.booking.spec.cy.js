2ab56acc75d403745a184ae4dee0c4fcea032fa3
Marijana Milovanovic <marijanajelenic92@gmail.com>
2025-03-01 00:10:33 +0100

    Update create.booking.spec.cy.js

--- cypress/e2e/create.booking.spec.cy.js
+++ cypress/e2e/create.booking.spec.cy.js
@@ -56,4 +56,52 @@ describe('Create booking with xml and with json format', () => {
     //exist bug,  content-type should be text/xml, in this case back text/html
   })
 
+  it('Create booking, try send wrong format in body (xml) and check response', () => {
+    const xmlBodyBooking = `
+    < booking >
+        <firstname>${userInfo.firstname}</firstname>
+        <lastname>${userInfo.lastname}</lastname>
+        <totalprice>${userInfo.totalprice}</totalprice>
+        <depositpaid>${userInfo.depositpaid}</depositpaid>
+        <additionalneeds>${userInfo.additionalneeds}</additionalneeds>
+        <bookingdates>
+            <checkin>${userInfo.checkin}</checkin>
+            <checkout>${userInfo.checkout}</checkout>
+        </bookingdates>
+    </booking >
+    `;
+    mainPage.createBooking(
+      urls.baseUrl + "booking",
+      400,
+      xmlBodyBooking,
+      'application/json',
+      'application/json',
+      'text/plain',
+      "invalid"
+    )
+  })
+
+  it('Create booking, try send wrong format in body (json) and check response', () => {
+    const jsonBodyBooking =
+    {
+      "firstname": userInfo.firstname,
+      "lastname": userInfo.lastname,
+      "totalprice": userInfo.totalprice,
+      "depositpaid": userInfo.depositpaid,
+      "additionalneeds": userInfo.additionalneeds,
+      "bookingdates": {
+        "checkin": userInfo.checkin,
+        "checkout": userInfo.checkout
+      }
+    };
+    mainPage.createBooking(
+      urls.baseUrl + "booking",
+      400,
+      jsonBodyBooking,
+      'text/xml',
+      'application/xml',
+      'text/plain',
+      "invalid")
+  })
+
 })
